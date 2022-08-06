b301b99be6b7b1a234f0fc5955c5f00ebf351948
K-Sato1995 <katsuki_sato0130@icloud.com>
2022-08-06 21:34:44 +0900

    Get a random image from unsplash

--- api/ogp.go
+++ api/ogp.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -16,6 +18,30 @@ func init() {
 	}
 }
 
-func Print() {
-	fmt.Println(os.Getenv("NAME"), os.Getenv("EDITOR"))
+const END_POINT = "https://api.unsplash.com/photos/random"
+
+func httpClient() *http.Client {
+	return &http.Client{}
+}
+
+func FetchImage() string {
+	var ACCESS_KEY = os.Getenv("UNSPLASH_ACCESS_KEY")
+	client := httpClient()
+
+	req, err := http.NewRequest("GET", END_POINT, nil)
+
+	req.Header.Add("Authorization", "Client-ID "+ACCESS_KEY)
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		var errMsg = fmt.Sprintf("Failed to fetch: %s", err)
+		log.Fatal(errMsg)
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+
+	sb := string(body)
+	return sb
 }
--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"ogp-server/api"
 )
 
 func main() {
-	api.Print()
+	fmt.Println(api.FetchImage())
 }
